779112cabfd825117faae25fbf8be94ad6c04177
Ahmed Aladdin <ahmed.ibrahim035@eng-st.cu.edu.eg>
2023-07-29 07:58:48 +0300

    pages scrolling

--- js/page.js
+++ js/page.js
@@ -1,10 +1,22 @@
 const main = document.querySelector('main');
+let x = 0;
+const limit = 300;
 main.addEventListener('wheel', event=> {
   event.preventDefault();
 
-  if(event.deltaY > 0)
-    main.scrollLeft += main.offsetWidth/3;
-  else
-    main.scrollLeft -= main.offsetWidth/3;
+  x += event.deltaY;
+
+  if(x > limit)
+  {
+    main.scrollLeft += main.scrollWidth/3.5;
+    x = 0;
+  }
+  else if ( x < -limit)
+  {
+    main.scrollLeft -= main.scrollWidth/3.5;
+    x = 0;
+  }
+
+  console.log(event.deltaY);
 
 });
