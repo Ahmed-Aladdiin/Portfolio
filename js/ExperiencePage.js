26365905363134788b4bc56b15d943f076a415e5
Ahmed Aladdin <ahmed.ibrahim035@eng-st.cu.edu.eg>
2023-08-05 11:45:01 +0300

    Apply the images scrolling paralex

--- js/ExperiencePage.js
+++ js/ExperiencePage.js
@@ -1,8 +1,11 @@
 const track = document.querySelector('#imgs-track');
+const images = document.querySelectorAll("#imgs-track img");
 
 track.addEventListener('scroll', (e) => {
   
   let percentage = Math.round(track.scrollLeft/(track.scrollWidth - track.clientWidth) * 100);
 
-  console.log(Math.round(track.scrollLeft), track.scrollWidth - track.clientWidth, percentage);
+  images.forEach(image => {
+    image.animate({objectPosition: `${100 - percentage}% 50%`}, {duration: 700, fill: 'forwards'});    
+  });
 })
