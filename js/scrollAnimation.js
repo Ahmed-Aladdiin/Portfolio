d9358797af83c0fe019ccfa89b410aabf319258e
Ahmed Aladdin <ahmed.ibrahim035@eng-st.cu.edu.eg>
2023-08-09 11:09:58 +0300

    change focus to track when entering the experience page

--- js/scrollAnimation.js
+++ js/scrollAnimation.js
@@ -25,3 +25,14 @@ const hiddenProjects = document.querySelectorAll('.hide-2');
 hiddenSkillsLeft.forEach( el => skillsObserver.observe(el) );
 hiddenSkillsRight.forEach( el => skillsObserver.observe(el) );
 hiddenProjects.forEach( el => skillsObserver.observe(el) );
+
+// change focus to the track
+window.addEventListener('wheel', ()=>{
+  if(pos == 2)
+    track.focus();
+  else
+    track.blur();
+  console.log(pos)
+  console.log(document.activeElement)
+  // console.log(track);
+});
